c6d3ee84fddeab5aa966c6e37c8952640df8ef7b
gulsum <gulsum.akyildizz@gmail.com>
2018-06-13 14:57:09 +0300

    Unity 3d ile bir model etrafında dönme (mouse or touch)

---
+++ Assets/rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+	public GameObject man;
+	float speed;
+	float rotatex;
+	private readonly string  mousex="Mouse X";
+	// Use this for initialization
+	void Start () {
+		speed = 1000;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		rotatex = Input.GetAxis (mousex) * speed * Mathf.Deg2Rad;
+		transform.RotateAround (man.transform.position, Vector3.up, rotatex);
+	}
+
+}
