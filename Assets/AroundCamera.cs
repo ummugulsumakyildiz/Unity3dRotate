21d4ac27ff84eb0d7837e6c209c736fac871982e
gulsum <gulsum.akyildizz@gmail.com>
2018-06-27 20:50:26 +0300

    AroundCamera ve CameraController scripleri

---
+++ Assets/AroundCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AroundCamera : MonoBehaviour {
+
+	public GameObject target;
+	private float speedMod = 10.0f;
+	private Vector3 point;
+
+	// Use this for initialization
+	void Start () {
+		point = target.transform.position;
+		transform.LookAt (point);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (target.transform.position,  Vector3.up, speedMod * Time.deltaTime);
+	}
+}
---
+++ Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public GameObject man;
+	private Vector3 offset;
+
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - man.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = man.transform.position + offset;
+	}
+}
